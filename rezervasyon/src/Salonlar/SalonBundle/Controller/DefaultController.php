714becd42e232b0af3bcf5b11de0e88e4ad407e9
sercanKuliq <couper0@gmail.com>
2016-09-21 17:11:02 +0300

    commited

--- rezervasyon/src/Salonlar/SalonBundle/Controller/DefaultController.php
+++ rezervasyon/src/Salonlar/SalonBundle/Controller/DefaultController.php
@@ -2,9 +2,11 @@
 
 namespace Salonlar\SalonBundle\Controller;
 
+use Salonlar\SalonBundle\Entity\Salon1;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 
@@ -17,9 +19,50 @@ class DefaultController extends Controller
      * @return Response
      */
     public function writeAction(Request $request){
-        $name = $request->request->get('name');
-        $lastname = $request->request->get('lastname');
+        if($request->isMethod('post')){
+            $title = $request->get('title');
+            $content = $request->get('content');
 
-        return['name' => $name, 'lastname' => $lastname];
+            $salon1 = new Salon1();
+            $salon1->setTitle($title)
+                ->setContent($content);
+
+            //kaydetme
+            $en = $this->getDoctrine()->getManager();
+
+            $en->persist($salon1);
+            $en->flush();
+            return["message" => "başarılı"];
+        }
+        return [];
+    }
+
+    /**
+     * @return array
+     * @Route("/salon/list",name="salon_list")
+     * @Template
+     */
+    public function listAction(){
+        $en = $this->getDoctrine()->getManager();
+        $repo = $en->getRepository("SalonBundle:Salon1");
+        $items = $repo->findAll();
+        return ['items' => $items];
+    }
+
+    /**
+     * @param $id
+     * @Route("/salon/delete/(salon1)", requirement("salon1";"id+"), name=" salon_delete")
+     * @Template
+     */
+    public function deleteAction(Salon1 $salon1){
+        $en = $this->getDoctrine()->getManager();
+        $en->remove($salon1);
+
+        //mesaj gönerme
+        $session = $this->get('session');
+        $flashbag = $session->getFlashBag();
+        $flashbag->add("success","İçerik başarıyla silindi");
+        $session->save();
+        return RedirectResponse::create("/salon/list");
     }
 }
