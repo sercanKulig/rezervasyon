c41f5e04a649df0e8f182fc0c913403e2b6cf29f
sercanKuliq <couper0@gmail.com>
2016-09-21 15:02:57 +0300

    commited

--- rezervasyon/src/Salonlar/SalonBundle/Controller/DefaultController.php
+++ rezervasyon/src/Salonlar/SalonBundle/Controller/DefaultController.php
@@ -2,20 +2,24 @@
 
 namespace Salonlar\SalonBundle\Controller;
 
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
-use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 
 class DefaultController extends Controller
 {
     /**
-     * @Route("/salon/kayit",name="salon_kayit")
      * @Template
+     * @param Request $request
+     * @Route("/salon/write",name="salon_write")
+     * @return Response
      */
-    public function kayitAction(){
-        $response = new Response();
-        $response->setContent("");
-        return $response;
+    public function writeAction(Request $request){
+        $name = $request->request->get('name');
+        $lastname = $request->request->get('lastname');
+
+        return['name' => $name, 'lastname' => $lastname];
     }
 }
