aeb3011488fc79108b87837b5a61d47e2e826942
sercanKuliq <couper0@gmail.com>
2016-09-21 14:26:11 +0300

    (commited)

--- rezervasyon/src/Salonlar/SalonBundle/Controller/DefaultController.php
+++ rezervasyon/src/Salonlar/SalonBundle/Controller/DefaultController.php
@@ -4,14 +4,18 @@ namespace Salonlar\SalonBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+use Symfony\Component\HttpFoundation\Response;
 
 class DefaultController extends Controller
 {
     /**
-     * @Route("/salon/kayit",name="salon_kayit)
+     * @Route("/salon/kayit",name="salon_kayit")
+     * @Template
      */
-    public function indexAction()
-    {
-        return $this->render('SalonBundle:Default:index.html.twig');
+    public function kayitAction(){
+        $response = new Response();
+        $response->setContent("");
+        return $response;
     }
 }
