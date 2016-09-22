0e19767e57912f7f4e7033b00db43255621ab0cf
sercanKuliq <couper0@gmail.com>
2016-09-23 02:32:04 +0300

    commit

--- rezervasyon/src/Salonlar/SalonBundle/Controller/PersonController.php
+++ rezervasyon/src/Salonlar/SalonBundle/Controller/PersonController.php
@@ -9,9 +9,12 @@
 namespace Salonlar\SalonBundle\Controller;
 
 use Salonlar\SalonBundle\Entity\customer;
+use Salonlar\SalonBundle\Repository\CustomerRepository;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
@@ -25,8 +28,55 @@ class PersonController extends Controller
      * @return Response
      */
 
-    public function showAction(Request $request){
-        if($request->isMethod('POST')){
+    public function addAction(Request $request){
+
+        $request = $this->get('request_stack')->getCurrentRequest();
+        $em = $this->getDoctrine()->getManager();
+        /**@var CustomerRepository $repo*/
+        $repo = $em->getRepository('SalonBundle:customer');
+
+        $form = $repo->newPerson();
+
+        $formBuild = $this->createFormBuilder($form)
+            ->setMethod('POST')
+            ->setAction('listing')
+            ->add('name',TextType::class,[
+                'label' =>' ',
+                'attr'=>[
+                    'class' => 'form-control',
+                    'type'=>'text',
+                    'placeholder' =>'İsim ve Soyad',
+                    'required' => 'required',
+                ]
+            ])
+            ->add('email',TextType::class,[
+                'label' =>' ',
+                'attr'=>[
+                    'class' => 'form-control',
+                    'type'=>'email',
+                    'placeholder' =>'Email',
+                    'required' => 'required',
+                ]
+            ])
+            ->add('phone',TextType::class,[
+                'label' =>' ',
+                'attr'=>[
+                    'class' => 'form-control',
+                    'type'=>'number',
+                    'placeholder' =>'Telefon',
+                    'required' => 'required',
+                ]
+            ])->add('submit',SubmitType::class,[
+                'label' => 'gönder',
+                'attr'=>[
+                'class' => 'btn btn-default'
+            ]
+            ])
+            ->getForm();
+        ;
+
+        $formBuild->handleRequest($request);
+        if($formBuild->isSubmitted() && $formBuild->isValid()){
             $name = $request->get('name');
             $email = $request->get('email');
             $phone = $request->get('phone');
@@ -37,11 +87,19 @@ class PersonController extends Controller
                 ->setPhone($phone);
 
             //kaydetme
-            $em = $this->getDoctrine()->getManager();
             $em->persist($customer);
             $em->flush();
             return["message" => "başarılı"];
         }
-        return [];
+        return [
+            'form' => $formBuild->createView()
+        ];
+    }
+
+    public function showAction(){
+        $em = $this->getDoctrine()->getManager();
+        $repo = $em->getRepository("SalonBundle:customer");
+        $items = $repo->findAll();
+        return ['items'=>$items];
     }
 }
